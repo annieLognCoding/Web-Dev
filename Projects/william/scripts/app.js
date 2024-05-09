a353cef81e9a0ce15e160ca19cf15af7d0f904c2
june0803 <annie.chang@logncoding.com>
2024-05-09 14:34:21 +0900

    dd

---
+++ Projects/william/scripts/app.js
@@ -0,0 +1,16 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByTagName("button");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Click on the first tab by default
+document.getElementsByClassName("tablink")[0].click();
